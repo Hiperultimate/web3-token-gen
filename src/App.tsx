b8b0932273e334d84f91426bfbd4828b10433f5f
Hiperultimate <hiperultimatevegito@gmail.com>
2025-06-10 17:55:41 +0530

    Setting up solana wallet adapter

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
+import "@solana/wallet-adapter-react-ui/styles.css";
+
 import { ThemeProvider } from "@/components/theme-provider";
-import { Toaster } from "@/components/ui/sonner"
+import { Toaster } from "@/components/ui/sonner";
 import HomePage from "./components/page/home-page";
 import "./App.css";
+import { SolanaProvider } from "./components/solana-provider";
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <HomePage />
-      <Toaster />
-    </ThemeProvider>
+    <SolanaProvider>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <HomePage />
+        <Toaster />
+      </ThemeProvider>
+    </SolanaProvider>
   );
 }
 
--- src/components/page/home-page.tsx
+++ src/components/page/home-page.tsx
@@ -1,7 +1,15 @@
-function HomePage() {
+import {
+  WalletMultiButton,
+  WalletDisconnectButton,
+} from "@solana/wallet-adapter-react-ui";
 
+function HomePage() {
   return (
     <div>
+      <div className="flex gap-2">
+        <WalletMultiButton />
+        <WalletDisconnectButton />
+      </div>
       Home page
     </div>
   );
